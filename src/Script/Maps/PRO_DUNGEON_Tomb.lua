9b4d1e2034578c66e9c770288bf46ef373a74f01
Jeroen Broks <facebook@tbbs.nl>
2016-11-04 12:36:45 +0100

    Fri Nov  4 12:36:45 CET 2016  Puzzle Tomb Set Up

--- src/Script/Maps/PRO_DUNGEON_Tomb.lua
+++ src/Script/Maps/PRO_DUNGEON_Tomb.lua
@@ -1,6 +1,8 @@
 -- Property of Jeroen P. Broks
 -- May not be extracted from this game.
 
+DonePuzzle = "&DONE.FEENALARIA.TOMB.PUZZLE.MATCH.SOLVED"
+
 function MapMusic()
    if not Done("&DONE.SANDRA.OPEN.DANDOR.PROLOGUE") then 
       CSay("Skip the music for now. Let John speak first.")
@@ -28,8 +30,38 @@ function Sandra()
 end
 
 function InitPuzzle()
+   if CVV(DonePuzzle) or Symbols then return end
+   local r
    Symbols = {}
    for f in iJCR6Dir(true) do
        if prefixed(f,'GFX/TEXTURES/SHAPES/') and suffixed(f,".PNG") then Symbols[#Symbols+1] = f end
    end
+   assert(#Symbols>=16,"Not enough Symbols. 16 at least required.")
+   Tiles = {}
+   local SymUsed = {}
+   for i=1,16 do
+       local timeout=1000
+       repeat
+          timeout=timeout-1
+          assert(timeout>=0,"Puzzle Generation TimeOut! (Start)")
+          r = rand(1,#Symbols)
+       until not SymUsed[r]
+       SymUsed[r] = i
+       Tiles[i] = Symbols[r]
+   end
+   same = {}
+   timeout=1000
+   repeat
+      timeout=timeout-1
+      assert(timeout>=0,"Puzzle Generation TimeOut! (Same)")
+      same.ori = rand(1,16)
+      same.tgt = rand(1,16)
+   until same.ori~=same.tgt
+   for i=1,16 do
+       Maps.Obj.Obj("Sym"..i).TextureFile = Tiles[i]
+   end          
+end
+
+function GALE_OnLoad()
+   ZA_Enter('PuzzleField',InitPuzzle)
 end
