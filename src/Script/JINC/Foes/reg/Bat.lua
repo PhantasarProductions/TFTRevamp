60c84246e0293fe1475d4bb1572ce039d61f2f48
Jeroen Broks <facebook@tbbs.nl>
2016-11-17 16:57:09 +0100

    Thu Nov 17 16:57:08 CET 2016  Shadow Sweeper

--- src/Script/JINC/Foes/reg/Bat.lua
+++ src/Script/JINC/Foes/reg/Bat.lua
@@ -34,7 +34,7 @@
      misrepresented as being the original software.
   3. This notice may not be removed or altered from any source distribution.
 ]]
--- File Generated: Thu 17 November 2016; 12:00:10
+-- File Generated: Thu 17 November 2016; 16:56:18
 
 
 local foe = {}
