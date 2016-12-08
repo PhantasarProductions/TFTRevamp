50057d4e7630f8417ab087d1faac7e06ae5c9aea
Jeroen Broks <facebook@tbbs.nl>
2016-12-08 14:27:45 +0100

    Thu Dec  8 14:27:44 CET 2016  Additional push as the fix on the SFX was not saved

--- src/Script/Subs/SFX.lua
+++ src/Script/Subs/SFX.lua
@@ -29,10 +29,12 @@ return ret
 end
 
 -- Play sound effect
-function SFX(pfile,channel,once) --,force)
+function SFX(pfile,pchannel,once) --,force)
 local file = Str.Upper(pfile)
 SFXBuffers = SFXBuffers or {}
 SFXChannels = SFXChannels or {}
+local channel = pchannel
+if pchannel = 'nil' then channel=nil end
 --[[
 -- force should ALWAYS be used from the title menu, otherwise... CRASH!!!!
 if GameScript and not config.sfx then Console.Write('Sound Effects are OFF, so request to play '..file..' has been ignored',255,0,0); return end -- if in a gamescript and the config is set to no sfx, then no sfx :)
