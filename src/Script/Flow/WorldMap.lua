8a0601286ae75585d53f97e6ed34938ae649c06e
Jeroen Broks <facebook@tbbs.nl>
2016-12-09 12:21:14 +0100

    Fri Dec  9 12:21:13 CET 2016  World map loader

--- src/Script/Flow/WorldMap.lua
+++ src/Script/Flow/WorldMap.lua
@@ -1,5 +1,26 @@
 wm_unlocked = wm_unlocked or {}
 
 dotcol = { nodung =   {  0,  0,255 },
-           doemblem = {255,  0,  0 },
-           emblem =   {  0,255,  0 }} 
+           noemblem = {255,  0,  0 },
+           emblem =   {  0,255,  0 }}
+           
+           
+           
+function LoadWorld(worldfolder)
+   allwords = allwords or jinc('Script/JINC/Big/WorldMap.lua')
+   world = {}
+   for k,v in pairs(allwords) do
+       if v.Folder==worldfolder then
+          world[v.LocationName] = v
+          if v.Dungeon then
+             if CVVN("&ALLOW.ENCOFF['"..v.Kthura.."']") then 
+                v.Dot = dotcol.emblem
+             else
+                v.Dot = dotcol.noemblem
+             end
+          else
+             v.Dot = dotcol.nodung
+          end
+       end
+   end
+end            
