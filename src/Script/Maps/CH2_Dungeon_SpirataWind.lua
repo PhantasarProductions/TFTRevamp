e93f2b2b804cc73e312c3fd6933c07e913d2a449
Jeroen Broks <facebook@tbbs.nl>
2017-02-18 10:13:43 +0100

    Sat Feb 18 10:13:42 CET 2017  Wind puzzle works until the moment the system has to check if the puzzle has been solved or not and until the moment you actually 'blew' it (get it, get it? hahaha!) by exceeding the max wind force

--- src/Script/Maps/CH2_Dungeon_SpirataWind.lua
+++ src/Script/Maps/CH2_Dungeon_SpirataWind.lua
@@ -37,13 +37,79 @@ version: 17.02.17
 
 -- @USE /script/use/specific/plasmafloor.lua
 
+function GenPuzzle()
+   local l=Maps.LayerCodeName
+   local v="&DONE.SPIRATA.WIND.PUZZLE["..l.."]"
+   local c={[true]={180,0,0},[false]={0,180,0}}
+   if CVV(v) then return end
+   local v=rand(3,12)
+   for i=1,12 do      
+         for let in each({"A","B"}) do
+             local o=Maps.Obj.Obj(let..i)
+             local goed=i==v 
+             o.R=c[goed][1] 
+             o.G=c[goed][2] 
+             o.B=c[goed][3]
+         end     
+   end
+   puzdata = {
+     goedeantwoord=v,
+     meter=-3,
+     balletje=Maps.Obj.Obj('Balletje').Y,
+     ondergrens=Maps.Obj.Obj("A1").Y+32
+   }
+   CSay("Correct answer is "..v)
+   CSay(serialize("Generated data ",puzdata))
+end
+
+function TurnDone()
+     local d=Maps.Obj.Obj("Obstacle_Done")
+     d.Rotation = d.Rotation + 1
+     if d.Rotation>=360 then d.Rotation = d.Rotation - 360 end 
+end
+
+function POST_Controls()
+  local v = Sys.Val(AltInputResult()) -- Sys.Val GUARANTEES a number, where tonumber can return 'nil', and cannot chance that latter option.
+  if v<=0 then
+     puzdata.meter=-3
+  elseif v>12 then
+     puzdata.meter=100 -- Make sure the ball will fly out!
+  else
+     puzdata.meter=v   
+  end  
+CSay(serialize("Current Puzzle Data",puzdata))    
+end
+
+function NPC_Controls()
+    if not puzdata then return end
+    Schedule('MAP','POST_Controls')
+    AltInput('Desired Wind Force:','','num')
+end
+
+function PuzRoom()
+   if not puzdata then return end
+   local balletje = Maps.Obj.Obj("Balletje")
+   TurnDone()
+   balletje.R=rand(0,255)
+   balletje.G=rand(0,255)
+   balletje.B=rand(0,255)
+   local y = puzdata.ondergrens
+   y = y - ((puzdata.meter-1)*32)
+   y = y + (math.sin(Time.MSecs()/2)*4)
+   if     balletje.Y<y then balletje.Y = balletje.Y + 2 
+   elseif balletje.Y>y then balletje.Y = balletje.Y - 2 end
+end
+
+
+SubFlow = { ['#004']=PuzRoom}
+
 function MAP_FLOW()
    --FlowBlops()
    FlowMoveTex(hsin)
    FlowMoveTex(vsin)
    FlowMoveTex(wind)
    local lay = Maps.LayerCodeName;
-   -- (SubFlow[lay] or Nothing)()
+   (SubFlow[lay] or Nothing)()
 end
 
 function GALE_OnLoad()
@@ -52,11 +118,10 @@ function GALE_OnLoad()
    vsin = InitMoveTex({X= 0,Y=1,suffix='VSINUS.PNG'})
    wind = InitMoveTex({X=-4,Y=0,tag='WindLeft'})
    --[[
-   ZA_Enter('GenPuz2',GeneratePuzzle,4)
-   ZA_Enter('GenPuz8',GeneratePuzzle,8)
    ZA_Enter('SpirataRoom',Music,'Hub/Angevin.ogg')
    ZA_Enter('Kerk'       ,Music,"Dungeon/Chanson d'eglise.ogg")
    ZA_Enter('Spirata',Spirata)
    ]]
+   ZA_Enter('GenPuzzle',GenPuzzle)
    MapHide('Secret')
 end
