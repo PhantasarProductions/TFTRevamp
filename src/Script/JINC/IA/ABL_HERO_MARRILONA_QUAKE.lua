955549152af3fa15103a8e28167d3baaeca6c498
Jeroen Broks <facebook@tbbs.nl>
2017-03-29 23:30:33 +0200

    Wed Mar 29 23:30:33 CEST 2017  Earthquake

---
+++ src/Script/JINC/IA/ABL_HERO_MARRILONA_QUAKE.lua
@@ -0,0 +1,70 @@
+--[[
+  ABL_HERO_MARRILONA_QUAKE.lua
+  Version: 17.03.29
+  Copyright (C) 2017 Jeroen Petrus Broks
+  
+  ===========================
+  This file is part of a project related to the Phantasar Chronicles or another
+  series or saga which is property of Jeroen P. Broks.
+  This means that it may contain references to a story-line plus characters
+  which are property of Jeroen Broks. These references may only be distributed
+  along with an unmodified version of the game. 
+  
+  As soon as you remove or replace ALL references to the storyline or character
+  references, or any termology specifically set up for the Phantasar universe,
+  or any other univers a story of Jeroen P. Broks is set up for,
+  the restrictions of this file are removed and will automatically become
+  zLib licensed (see below).
+  
+  Please note that doing so counts as a modification and must be marked as such
+  in accordance to the zLib license.
+  ===========================
+  zLib license terms:
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+]]
+ret = {
+	["ABL_APCost"] = 150,
+	["ADDCARD_Action_Act"] = "Self",
+	["ADDCARD_Action_Interval"] = 50,
+	["ADDCARD_Action_Number"] = 2,
+	["Attack"] = 200,
+	["Attack_AccuracyRate"] = 100,
+	["Attack_AllowCritical"] = true,
+	["Attack_AttackStat"] = "Intelligence",
+	["Attack_DefenseStat"] = "Resistance",
+	["Attack_Element"] = "Earth",
+	["Desc"] = "It's the arockalypse, now bear your soul",
+	["Heal_StatPercent"] = "Intelligence",
+	["Heal_Type"] = "Absolute",
+	["ITM_ACC_Dandor"] = true,
+	["ITM_ACC_HandoStillor"] = true,
+	["ITM_ACC_Jake"] = true,
+	["ITM_ACC_Marrilona"] = true,
+	["ITM_Combat"] = true,
+	["ITM_EQP_For"] = "Jake",
+	["ITM_Field"] = true,
+	["ITM_Sellable"] = true,
+	["ITM_Type"] = "Consumable",
+	["SpellAni"] = "Quake",
+	["Stance"] = "Cast",
+	["Target"] = "1A",
+	["Title"] = "Earthquake",
+	["Type"] = "Ability",
+	["rew_GainSkill5"] = 250}
+
+return ret
+
+-- This file is an automatically generated file!
+
