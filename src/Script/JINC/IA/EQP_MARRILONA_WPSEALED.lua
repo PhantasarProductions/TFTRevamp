db81a676bafcb17d3e19b5e08800aba5b9c35bda
Tricky1975 <facebook@tbbs.nl>
2017-06-26 20:28:01 +0200

    Mon Jun 26 20:28:01 CEST 2017  Catastrophe - #13

---
+++ src/Script/JINC/IA/EQP_MARRILONA_WPSEALED.lua
@@ -0,0 +1,104 @@
+--[[
+**********************************************
+  
+  EQP_MARRILONA_WPSEALED.lua
+  (c) Jeroen Broks, 2017, All Rights Reserved.
+  
+  This file contains material that is related 
+  to a storyline that is which is strictly
+  copyrighted to Jeroen Broks.
+  
+  This file may only be used in an unmodified
+  form with an unmodified version of the 
+  software this file belongs to.
+  
+  You may use this file for your study to see
+  how I solved certain things in the creation
+  of this project to see if you find valuable
+  leads for the creation of your own.
+  
+  Mostly this file comes along with a project
+  that is for most part released under an
+  open source license and that means that if
+  you use that code with this file removed
+  from it, you can use it under that license.
+  Please check out the other files to find out
+  which license applies.
+  This file comes 'as-is' and in no possible
+  way the author can be held responsible of
+  any form of damages that may occur due to 
+  the usage of this file
+  
+  
+ **********************************************
+ 
+version: 17.06.26
+]]
+ret = {
+	["ADDCARD_Action_Act"] = "Self",
+	["ADDCARD_Action_Interval"] = 200,
+	["ADDCARD_Action_Number"] = 5,
+	["ADDCARD_Char_Interval"] = 5,
+	["Attack"] = 125,
+	["Attack_AccuracyRate"] = 100,
+	["Attack_AllowAccuracy"] = true,
+	["Attack_AllowCritical"] = true,
+	["Attack_AttackStat"] = "Intelligence",
+	["Attack_DefenseStat"] = "Resistance",
+	["Attack_Element"] = "None",
+	["BUFF_Resistance"] = -10,
+	["CauseCurse"] = true,
+	["Desc"] = "None shall live!",
+	["EQP_STAT_AP"] = 500,
+	["EQP_STAT_Accuracy"] = 25,
+	["EQP_STAT_ER_Darkness"] = 10,
+	["EQP_STAT_ER_Earth"] = 5,
+	["EQP_STAT_ER_Flame"] = 5,
+	["EQP_STAT_ER_Frost"] = 5,
+	["EQP_STAT_ER_Light"] = 5,
+	["EQP_STAT_ER_Lightning"] = 5,
+	["EQP_STAT_ER_Water"] = 5,
+	["EQP_STAT_ER_Wind"] = 5,
+	["EQP_STAT_Endurance"] = 5,
+	["EQP_STAT_Evasion"] = 5,
+	["EQP_STAT_HP"] = 5,
+	["EQP_STAT_Intelligence"] = 500,
+	["EQP_STAT_Power"] = 5,
+	["EQP_STAT_Resistance"] = 250,
+	["EQP_STAT_SR_Curse"] = 25,
+	["EQP_STAT_SR_Death"] = 5,
+	["EQP_STAT_SR_Destruction"] = 5,
+	["EQP_STAT_SR_Disease"] = 5,
+	["EQP_STAT_SR_Paralysis"] = 5,
+	["EQP_STAT_SR_Petrification"] = 10,
+	["EQP_STAT_SR_Poison"] = 5,
+	["EQP_STAT_SR_Silence"] = 5,
+	["EQP_STAT_SR_Sleep"] = 5,
+	["EQP_STAT_SR_Undead"] = 5,
+	["Heal_StatPercent"] = "Intelligence",
+	["ITM_ACC_Dandor"] = true,
+	["ITM_ACC_HandoStillor"] = true,
+	["ITM_ACC_Jake"] = true,
+	["ITM_ACC_Marrilona"] = true,
+	["ITM_Combat"] = true,
+	["ITM_EQP_For"] = "Marrilona",
+	["ITM_Field"] = true,
+	["ITM_Type"] = "Weapon",
+	["ITM_WeaponAbility"] = true,
+	["Rew_GainAP"] = 25,
+	["SpellAni"] = "AllHeal",
+	["Stance"] = "Cast",
+	["Target"] = "AF",
+	["Title"] = "Catastrophe",
+	["Type"] = "Item",
+	["Voice"] = "Critical",
+	["rew_GainSkill1"] = 100,
+	["rew_GainSkill2"] = 100,
+	["rew_GainSkill3"] = 100,
+	["rew_GainSkill4"] = 100,
+	["rew_GainSkill5"] = 100}
+
+return ret
+
+-- This file is an automatically generated file!
+
