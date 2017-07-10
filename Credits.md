47639a42fa6fc4d5de500c025a2168b215fdf0ab
Tricky1975 <facebook@tbbs.nl>
2017-07-10 10:29:26 +0200

    Mon Jul 10 10:29:25 CEST 2017  Implemented and thus closed #330

--- Credits.md
+++ Credits.md
@@ -391,6 +391,7 @@ Please note all 3rd party contributions remain copyrighted and licensed by their
     1. Script: Script/JINC/IA/ABL_FOE_HORROR.lua
     1. Script: Script/JINC/IA/ABL_FOE_LIFEDRAIN.lua
     1. Script: Script/JINC/IA/ABL_FOE_LIFEFORCE.lua
+    1. Script: Script/JINC/IA/ABL_FOE_MASOSADO.lua
     1. Script: Script/JINC/IA/ABL_FOE_MERMAID_TEMPATION.lua
     1. Script: Script/JINC/IA/ABL_FOE_MULTISTRIKE.lua
     1. Script: Script/JINC/IA/ABL_FOE_PARADIVE.lua
@@ -957,6 +958,7 @@ Please note all 3rd party contributions remain copyrighted and licensed by their
     1. Script: Script/Use/Combat/SpellScript/Jake Shift.lua
     1. Script: Script/Use/Combat/SpellScript/Juggernaut.lua
     1. Script: Script/Use/Combat/SpellScript/Marrilona vs Dwarf - The end.lua
+    1. Script: Script/Use/Combat/SpellScript/MidasCashEffects.lua
     1. Script: Script/Use/Combat/SpellScript/Nostramantu Goes Oversoul.lua
     1. Script: Script/Use/Combat/SpellScript/PlayWithPoints.lua
     1. Script: Script/Use/Combat/SpellScript/PunishUser.lua
@@ -1023,14 +1025,14 @@ Please note all 3rd party contributions remain copyrighted and licensed by their
     1. Script: Script/Libs/QuickJCR6.lua
     1. Script: Script/Libs/Quick String Routines.lua
     1. Script: Script/Libs/A few mathematical functions.lua
-    1. Script: Script/Libs/SerializeIntoSysVar.lua
-    1. Script: Script/Libs/tablecontains.lua
-    1. Script: Script/Libs/ClearTable.lua
-    1. Script: Script/Libs/mysplit.lua
-    1. Script: Script/Libs/join.lua
-    1. Script: Script/Libs/DarkText.lua
-    1. Script: Script/Libs/valstr.lua
     1. Script: Script/Libs/Blends.lua
+    1. Script: Script/Libs/tablecontains.lua
+    1. Script: Script/Libs/valstr.lua
+    1. Script: Script/Libs/DarkText.lua
+    1. Script: Script/Libs/join.lua
+    1. Script: Script/Libs/mysplit.lua
+    1. Script: Script/Libs/ClearTable.lua
+    1. Script: Script/Libs/SerializeIntoSysVar.lua
     1. Graphics: gfx/Combat/Fighters/Foe/Boss/Beholder.piko
     1. Graphics: gfx/Combat/Fighters/Foe/Boss/Beholder.png
     1. Graphics: GFX/COMBAT/FIGHTERS/FOE/REG/BANSHEE/Meisje nummer 2   .png
---
+++ src/Script/Use/Combat/SpellScript/MidasCashEffects.lua
@@ -0,0 +1,60 @@
+--[[
+  MidasCashEffects.lua
+  Version: 17.07.10
+  Copyright (C) 2017 Jeroen Petrus Broks
+  
+  ===========================
+  This file is part of a project related to the Phantasar Chronicles or another
+  series or saga which is property of Jeroen P. Broks.
+  This means that it may contain references to a story-line plus characters
+  which are property of Jeroen Broks. These references may only be distributed
+  along with an unmodified version of the game. 
+  
+  As soon as you remove or replace ALL references to the storyline or character
+  references, or any termology specifically set up for the Phantasar universe,
+  or any other univers a story of Jeroen P. Broks is set up for,
+  the restrictions of this file are removed and will automatically become
+  zLib licensed (see below).
+  
+  Please note that doing so counts as a modification and must be marked as such
+  in accordance to the zLib license.
+  ===========================
+  zLib license terms:
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+]]
+-- @IF IGNORE
+local SpellScript = {}
+-- @FI
+
+
+function SpellScript.HardForTheMoney(foetag,exe)
+     local foe = fighterbytag[foetag]
+     if foe.data.Boss then CSay('Bosses are always unaffected') return false end
+     if (not foe.exp) then CSay('Experience not set at all')    return false end
+     if foe.exp==0    then CSay('Foe does not give exp')        return false end
+     --if not foe.data.Cash then CSay('Target has no money') return false end
+     CSay('HFTM: Before: Cash='..sval(foe.data.Cash)..'; Experience='..foe.exp)
+     foe.data.Cash = (foe.data.Cash or 0) + (foe.exp * (4-skill))
+     foe.exp = 0
+     CSay('HFTM: After:  Cash='..sval(foe.data.Cash)..'; Experience='..foe.exp)
+     return true
+end
+
+
+
+
+-- @IF INGORE
+return SpellScript 
+-- @FI
