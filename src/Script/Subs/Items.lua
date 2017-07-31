55a77421492f1b2c9273855aca4d2fe3ecd0dd1a
Tricky1975 <facebook@tbbs.nl>
2017-07-31 17:23:36 +0200

    Mon Jul 31 17:23:36 CEST 2017  Fixed #555

--- src/Script/Subs/Items.lua
+++ src/Script/Subs/Items.lua
@@ -429,8 +429,8 @@ function ItemShowList(showfilter,enablefilter,char,psizes)
        ScrollMax(scrollid,y+50)
    end
    EndScroller(scrollid)
-   if (INP.KeyH(KEY_DOWN)==1 or joyhit(joydown) or mousehit(1)) and pos[scrollid]<#showitems[showfilter..(char or "")]  then pos[scrollid] = pos[scrollid] + 1 end 
-   if (INP.KeyH(KEY_UP  )==1 or joyhit(joyup  ) or mousehit(1)) and pos[scrollid]>                                   1  then pos[scrollid] = pos[scrollid] - 1 end 
+   if (INP.KeyH(KEY_DOWN)==1 or joyhit(joy_down) or mousehit(1)) and pos[scrollid]<#showitems[showfilter..(char or "")]  then pos[scrollid] = pos[scrollid] + 1 end 
+   if (INP.KeyH(KEY_UP  )==1 or joyhit(joy_up  ) or mousehit(1)) and pos[scrollid]>                                   1  then pos[scrollid] = pos[scrollid] - 1 end 
    if (INP.KeyH(KEY_SPACE)==1 or INP.KeyH(KEY_RETURN)==1 or INP.KeyH(KEY_ENTER)==1 or joyhit('CONFIRM')) then 
       Var.D("$SELECTEDITEM",showitems[showfilter..(char or "")][pos[scrollid]])
       CSay("Selected: "..Var.C('$SELECTEDITEM')) 
