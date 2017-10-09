6770a83f84e43fba89a652659bcef83f3290b054
Tricky1975 <facebook@tbbs.nl>
2017-10-09 21:23:38 +0200

    Gen Update

--- src/Script/JINC/Big/Credits.lua
+++ src/Script/JINC/Big/Credits.lua
@@ -32,7 +32,7 @@
   
  **********************************************
  
-version: 17.09.23
+version: 17.10.09
 ]]
 Image.GetViewport()
 local mx,my,mw,mh = Image.GVP_X, Image.GVP_Y, Image.GVP_W, Image.GVP_H
